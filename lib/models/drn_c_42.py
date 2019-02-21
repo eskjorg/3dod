2f9dd23487854fba8b10ca917dcc0d92d4578e1b
Eskil Jörgensen <eskil.jorgensen@gmail.com>
2019-02-21 11:19:11 +0100

    Add drn_C_42

---
+++ lib/models/drn_c_42.py
@@ -0,0 +1,9 @@
+from torch import nn
+from third_party import drn
+
+def get_encoder():
+    network = getattr(drn, "drn_c_42")(pretrained=True)
+    bottleneck_channels = network.fc.in_channels
+    network = nn.Sequential(*list(network.children())[:-2])
+    downsampling_factor = 8
+    return network, bottleneck_channels, downsampling_factor
