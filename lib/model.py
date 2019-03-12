Fix post_proc of layerwise weighting

lib/model.py
@@ -1,7 +1,8 @@
 """Neural network model."""
 
 from importlib import import_module
-from torch import nn, Tensor
+import torch
+from torch import nn
 from lib.utils import get_layers
 
 class Model(nn.Module):
@@ -40,14 +41,15 @@ class MultiTaskNet(nn.ModuleDict):
                                       upsampling=upsampling)
             # The loss weighting for that head
             if settings['loss'] == "CE" or weighting_mode == 'uniform':  # no weighting
-                task_weighting = nn.Module()
-                task_weighting.forward = lambda x: Tensor([0])
+                task_weighting = ZeroHead()
             elif weighting_mode == 'layer_wise':  # a.k.a. homoscedatic
                 task_weighting = LayerWeights(settings['n_layers'])
             elif weighting_mode == 'sample_wise':  # a.k.a. heteroscedastic
                 task_weighting = MultiTaskHead(in_channels=in_channels,
                                                out_channels=settings['n_layers'],
                                                upsampling=upsampling)
+            else:
+                raise NotImplementedError(weighting_mode)
             heads[name] = WeightedHead([task_head, task_weighting])
         super(MultiTaskNet, self).__init__(heads)
 
@@ -82,7 +84,11 @@ class WeightedHead(nn.ModuleList):
 class LayerWeights(nn.Module):
     def __init__(self, n_weights):
         super().__init__()
-        self.register_parameter('weighting', nn.Parameter(Tensor(n_weights)))
+        self.register_parameter('weighting', nn.Parameter(torch.zeros(n_weights)))
 
     def forward(self, x):
         return self.weighting
+
+class ZeroHead(nn.Module):
+    def forward(self, x):
+        return torch.Tensor([0])

lib/postprocessing/bbox2d_detection.py
@@ -34,15 +34,17 @@ class Runner(RunnerIf):
         Generator = getattr(maps, key.capitalize() + 'Generator')
         metadata = None
         data = Generator(self._configs, metadata, self._class_map, device=get_device()).decode(data)
-        data = data.permute(1, 2, 0).reshape(-1, data.shape[0]).squeeze().float()
+        data = data.permute(1, 2, 0).reshape(-1, data.shape[0]).float()
         # weights
-        if key == "cls" or self._configs.training.weighting_mode == 'uniform':
-            weights = torch.Tensor(data.shape[0])
-            weights.fill_(getattr(self._configs.postprocessing.bbox3d_estimation.weights, key, 0))
-        else:
-            weights = (-outputs[key][1][frame_index]).exp().detach()
-            if self._configs.training.weighting_mode == 'sample_wise':
-                weights = weights.permute(1, 2, 0).reshape(-1, weights.shape[0]).float()
+        weighting_mode = self._configs.training.weighting_mode
+        if key == "cls" or weighting_mode == 'uniform':
+            weight = getattr(self._configs.postprocessing.bbox3d_estimation.weights, key, 0)
+            weights = torch.full(size=data.shape, fill_value=weight)
+        elif weighting_mode == "layer_wise":
+            weights = (-outputs[key][1].detach()).exp().expand(data.shape)
+        elif weighting_mode == 'sample_wise':
+            weights = (-outputs[key][1][frame_index].detach()).exp()
+            weights = weights.permute(1, 2, 0).reshape(-1, weights.shape[0]).float()
         return data, weights
 
     def detect_class(self, frame_outputs, class_index):