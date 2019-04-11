59a7403d588daf32aa26887a138f29e5d7b167b8
Lucas Brynte <brynte@chalmers.se>
2019-04-12 01:02:43 +0200

    render_gt mods

--- preprocessing/rigidpose/render_gt_glumpy.py
+++ preprocessing/rigidpose/render_gt_glumpy.py
@@ -8,6 +8,8 @@ import os
 # sys.path.append('../..') # Relative to CWD
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))) # Relative to module, but cannot be used in notebooks
 
+from lib.rigidpose.sixd_toolkit.pysixd import inout
+from preprocessing.rigidpose.glumpy_renderer import Renderer
 from lib.utils import listdir_nohidden
 import yaml
 import numpy as np
@@ -21,39 +23,22 @@ if LINEMOD_FLAG:
     SIXD_PATH = '/home/lucas/datasets/pose-data/sixd/occluded-linemod-augmented2bb_instance_idx_fix'
     SUBSETS = [subset for subset in listdir_nohidden(SIXD_PATH) if subset.startswith('train') or subset.startswith('test')]
 else:
-    SIXD_PATH = '/home/lucas/datasets/pose-data/sixd/ycb-video'
+    SIXD_PATH = '/home/lucas/datasets/pose-data/sixd/ycb-video2'
     SUBSETS = ['data']
 
-
-def write_pose_tud(pose_path, R, t):
-    assert R.shape == (3,3)
-    assert t.shape == (3,1)
-    D = np.diag([1,-1,-1]);
-    R = np.dot(D, R)
-    t = np.dot(D, t)
-    os.makedirs(os.path.dirname(pose_path), exist_ok=True)
-    with open(pose_path, 'w+') as f:
-        lines = []
-        lines.append("image size ")
-        lines.append("640 480")
-        lines.append("999") # Dummy object ID
-        lines.append("rotation: ")
-        for row in R:
-            lines.append(' '.join(map(str, row)))
-        lines.append("center: ")
-        lines.append(' '.join(map(str, t.T[0])))
-        lines.append("extent: ") # Dummy extent values
-        lines.append(' '.join(map(str, [0.0, 0.0, 0.0])))
-        f.writelines([line+'\n' for line in lines])
 
 def read_yaml(path):
     with open(path, 'r') as f:
         return yaml.load(f, Loader=yaml.CLoader)
 
 models_info = read_yaml(os.path.join(SIXD_PATH, 'models', 'models_info.yml'))
+models = {}
+for obj_id in models_info:
+    models[obj_id] = inout.load_ply(os.path.join(SIXD_PATH, 'models', 'obj_{:02}.ply'.format(obj_id)))
 
 for subset in SUBSETS:
     for seq in sorted(listdir_nohidden(os.path.join(SIXD_PATH, subset))):
+        render_seg = False if LINEMOD_FLAG and subset == 'train_aug' else True
         render_instance_seg = False if LINEMOD_FLAG and subset == 'train_aug' else True
         render_corr = True
         render_normals = True if LINEMOD_FLAG else False
@@ -61,6 +46,7 @@ for subset in SUBSETS:
         render_rgb = True if LINEMOD_FLAG else False
 
         rgb_dir = os.path.join(SIXD_PATH, subset, seq, 'rgb')
+        seg_dir = os.path.join(SIXD_PATH, subset, seq, 'seg')
         instance_seg_dir = os.path.join(SIXD_PATH, subset, seq, 'instance_seg')
         corr_dir = os.path.join(SIXD_PATH, subset, seq, 'obj')
         normals_dir = os.path.join(SIXD_PATH, subset, seq, 'normals')
@@ -68,6 +54,10 @@ for subset in SUBSETS:
         rgb_rendered_dir = os.path.join(SIXD_PATH, subset, seq, 'rgb_rendered')
 
         if not DRY_RUN:
+            if render_seg:
+                if os.path.exists(seg_dir):
+                    shutil.rmtree(seg_dir)
+                os.makedirs(seg_dir)
             if render_instance_seg:
                 if os.path.exists(instance_seg_dir):
                     shutil.rmtree(instance_seg_dir)
@@ -90,69 +80,53 @@ for subset in SUBSETS:
                 os.makedirs(rgb_rendered_dir)
 
         gts = read_yaml(os.path.join(SIXD_PATH, subset, seq, 'gt.yml'))
+        infos = read_yaml(os.path.join(SIXD_PATH, subset, seq, 'info.yml'))
 
         fnames = list(sorted(listdir_nohidden(rgb_dir)))
         for j, fname in enumerate(fnames):
             img_idx = int(fname.split('.')[0])
 
-            if (j+1) % 100 == 0:
+            info = infos[img_idx]
+
+            if (j+1) % 1 == 0:
                 print("subset {}, seq {}, frame {}/{}".format(subset, seq, j+1, len(fnames)))
 
-            instance_seg_path = os.path.join(instance_seg_dir, fname) if render_instance_seg else '/dev/null'
-            corr_path = os.path.join(corr_dir, fname) if render_corr else '/dev/null'
-            normals_path = os.path.join(normals_dir, fname) if render_normals else '/dev/null'
-            depth_rendered_path = os.path.join(depth_rendered_dir, fname) if render_depth else '/dev/null'
-            rgb_rendered_path = os.path.join(rgb_rendered_dir, fname) if render_rgb else '/dev/null'
+            seg_path = os.path.join(seg_dir, fname) if render_seg else None
+            instance_seg_path = os.path.join(instance_seg_dir, fname) if render_instance_seg else None
+            corr_path = os.path.join(corr_dir, fname) if render_corr else None
+            normals_path = os.path.join(normals_dir, fname) if render_normals else None
+            depth_rendered_path = os.path.join(depth_rendered_dir, fname) if render_depth else None
+            rgb_rendered_path = os.path.join(rgb_rendered_dir, fname) if render_rgb else None
 
-            tmp_pose_dir = '/tmp/tmp_poses_for_rendering'
-            if not DRY_RUN:
-                if os.path.exists(tmp_pose_dir):
-                    shutil.rmtree(tmp_pose_dir)
-                os.makedirs(tmp_pose_dir)
-
-            instance_idx_list = []
-            pose_path_list = []
-            mesh_path_list = []
-            instance_idx = 0
+            obj_id_list = []
+            R_list = []
+            t_list = []
+            model_list = []
             for gt in gts[img_idx]:
-                instance_idx += 1
-                mesh_path = os.path.join(SIXD_PATH, 'models', 'obj_{:02d}.ply'.format(gt['obj_id']))
+                obj_id_list.append(gt['obj_id'])
+                R_list.append(np.array(gt['cam_R_m2c']).reshape((3, 3)))
+                t_list.append(np.array(gt['cam_t_m2c']).reshape((3,1)))
+                model_list.append(models[gt['obj_id']])
 
-                R = np.array(gt['cam_R_m2c']).reshape((3, 3))
-                t = np.array(gt['cam_t_m2c']).reshape((3,1))# * 1e-3
-                pose_path = os.path.join(tmp_pose_dir, '{:03d}'.format(instance_idx))
+            renderer = Renderer(
+                [480, 640],
+                np.reshape(info['cam_K'], (3, 3)),
+                clip_near = 100, # mm
+                clip_far = 10000, # mm
+            )
 
-                if not DRY_RUN or True:
-                    write_pose_tud(pose_path, R, t)
+            rgb, depth, seg, instance_seg, normal_map, corr_map = renderer.render(
+                model_list,
+                R_list,
+                t_list,
+                obj_id_list,
+                texture_map_list = None,
+                surf_color_list = None,
+                ambient_weight = 0.8,
+            )
 
-                instance_idx_list.append(instance_idx)
-                pose_path_list.append(pose_path)
-                mesh_path_list.append(mesh_path)
-
-            cmd_list = []
-            cmd_list.append("/home/lucas/poserenderer/render-gt")
-            cmd_list.append("--seg-path")
-            cmd_list.append(instance_seg_path)
-            cmd_list.append("--coord-path")
-            cmd_list.append(corr_path)
-            cmd_list.append("--normals-path")
-            cmd_list.append(normals_path)
-            cmd_list.append("--depth-path")
-            cmd_list.append(depth_rendered_path)
-            cmd_list.append("--rgb-path")
-            cmd_list.append(rgb_rendered_path)
-            cmd_list.append("--mesh-paths")
-            cmd_list.append(",".join(map(str, mesh_path_list)))
-            cmd_list.append("--object-ids")
-            cmd_list.append(",".join(map(str, instance_idx_list)))
-            cmd_list.append("--pose-paths")
-            cmd_list.append(",".join(pose_path_list))
-
-            cmd = ' '.join(cmd_list)
-
-
-            if not DRY_RUN:
-                exit_code = os.system(cmd)
-                if exit_code != 0:
-                    print("Fail!")
-                    sys.exit(exit_code)
+            # if not DRY_RUN:
+            #     exit_code = os.system(cmd)
+            #     if exit_code != 0:
+            #         print("Fail!")
+            #         sys.exit(exit_code)
