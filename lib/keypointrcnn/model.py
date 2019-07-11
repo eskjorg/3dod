71be1dc08d317c97cd0e4d913230b6e012e21408
Lucas Brynte <brynte@chalmers.se>
2019-07-11 12:41:08 +0200

    Minor refactoring.

--- lib/keypointrcnn/model.py
+++ lib/keypointrcnn/model.py
@@ -4,13 +4,15 @@ from lib.constants import NBR_KEYPOINTS
 
 
 def Model(configs):
-    return kptrcnn(pretrained=False,
-                   progress=True,
-                   # num_classes=1,
-                   num_classes=2,
-                   # num_classes=3,
-                   # num_classes=1 + max(configs.data.class_map.values()),
-                   num_keypoints=NBR_KEYPOINTS,
-                   # rpn_post_nms_top_n_train=1,
-                   rpn_post_nms_top_n_test=1,
-                   pretrained_backbone=True)
+    return kptrcnn(
+        pretrained=False,
+        progress=True,
+        # num_classes=1,
+        num_classes=2,
+        # num_classes=3,
+        # num_classes=1 + max(configs.data.class_map.values()),
+        num_keypoints=NBR_KEYPOINTS,
+        # rpn_post_nms_top_n_train=1,
+        rpn_post_nms_top_n_test=1,
+        pretrained_backbone=True,
+    )
