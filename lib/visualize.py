ba255350a1e66a318a19cbbd990b12152865d106
Lucas Brynte <brynte@chalmers.se>
2019-07-16 12:22:00 +0200

    Plot estimated pose

--- lib/visualize.py
+++ lib/visualize.py
@@ -17,6 +17,7 @@ from torch.utils.tensorboard import SummaryWriter
 from lib.constants import PYPLOT_DPI, BOX_SKELETON, CORNER_COLORS, KEYPOINT_COLORS
 from lib.constants import TV_MEAN, TV_STD
 from lib.utils import project_3d_pts, construct_3d_box, get_metadata, get_class_map
+from lib.rigidpose.pose_estimator import pflat, deg_cm_error
 
 class Visualizer:
     """Visualizer."""
@@ -39,37 +40,83 @@ class Visualizer:
     def report_score(self, epoch, score, mode):
         self._writer.add_scalar('score/{}'.format(mode), score, epoch)
 
+    def plot_bbox3d(self, ax, K, R, t, x_bounds, y_bounds, z_bounds, **kwargs):
+        if len(t.shape) == 1:
+            t = t[:,None]
+        corners = np.zeros((3, 8))
+
+        corners[:,0] = [x_bounds[1], y_bounds[1], z_bounds[1]]
+        corners[:,1] = [x_bounds[0], y_bounds[1], z_bounds[1]]
+        corners[:,2] = [x_bounds[0], y_bounds[0], z_bounds[1]]
+        corners[:,3] = [x_bounds[1], y_bounds[0], z_bounds[1]]
+        corners[:,4] = [x_bounds[1], y_bounds[1], z_bounds[0]]
+        corners[:,5] = [x_bounds[0], y_bounds[1], z_bounds[0]]
+        corners[:,6] = [x_bounds[0], y_bounds[0], z_bounds[0]]
+        corners[:,7] = [x_bounds[1], y_bounds[0], z_bounds[0]]
+
+        corner_colors = ['k', 'w', 'r', 'g', 'y', 'c', 'm', 'b']
+
+        U = pflat(K @ (R @ corners + t))
+        xs = np.array([[U[0,0], U[0, 1], U[0, 2], U[0,3], U[0,4], U[0,5], U[0,6], U[0,7], U[0,0], U[0,1], U[0,2], U[0,3]], [U[0,1], U[0, 2], U[0,3], U[0,0], U[0,5], U[0,6], U[0,7], U[0,4], U[0,4], U[0,5], U[0,6], U[0,7]]])
+        ys = np.array([[U[1,0], U[1,1], U[1,2], U[1,3], U[1,4], U[1,5], U[1,6], U[1,7], U[1,0], U[1,1], U[1,2], U[1,3]], [U[1,1], U[1,2], U[1,3], U[1,0], U[1,5], U[1,6], U[1,7], U[1,4], U[1,4], U[1,5], U[1,6], U[1,7]]])
+        ax.plot([xs[0,:], xs[1,:]], [ys[0,:], ys[1,:]], **kwargs)
+        corner_plot_args = []
+        for pt, col in zip(U.T, corner_colors):
+            corner_plot_args += [pt[0], pt[1], col+'.']
+        ax.plot(*corner_plot_args)
+
     def save_images(self, batch, cnn_outs, frame_results, mode, index, sample=-1):
         if not any(self._configs.visualization.values()):
             return
         calib = batch.calibration[sample]
+        K = calib[:,:3]
         # image_tensor = normalize(batch.input[sample], mean=-TV_MEAN/TV_STD, std=1/TV_STD)
         image_tensor = batch.input[sample] 
         # frame_id = batch.id[sample]
         annotations = batch.annotation[sample]
 
+        anno_lookup = dict(zip([anno.cls for anno in annotations], annotations))
+
         frame_id = batch.id[sample]
         detections = frame_results[frame_id]
 
-        # TODO
-        # TODO
-        # TODO
-        # TODO
-        # TODO
-        # TODO
-        # Visualize estimated pose
-        # Visualize estimated pose
-        # Visualize estimated pose
-        # Visualize estimated pose
-        # Visualize estimated pose
-        # TODO
-        # TODO
-        # TODO
-        # TODO
-        # TODO
-        # for det in detections:
-        #     print(det.keypoints)
-        #     print(det.pose_results)
+        def assert_single_class_and_get_id():
+            assert len(anno_lookup.keys()) == 1
+            class_id = list(anno_lookup.keys())[0]
+            assert len(detections) <= 1
+            if len(detections) == 1:
+                assert detections[0].cls == class_id
+            return class_id
+
+        def plot_poses(ax, class_ids, annotation, detection):
+            for class_id in class_ids:
+                class_label = self._class_map.label_from_id(class_id)
+                if class_id in anno_lookup:
+                    anno = anno_lookup[class_id]
+                    self.plot_bbox3d(ax, K, anno.rotation, anno.location.numpy(), *self._metadata['objects'][class_label]['bbox3d'], color='b', linestyle='-', linewidth=1)
+            for det in detections:
+                if det.cls == class_id and det.pose_results is not None:
+                        self.plot_bbox3d(ax, K, det.pose_results['P_est'][:,:3], det.pose_results['P_est'][:,3], *self._metadata['objects'][class_label]['bbox3d'], color='g', linestyle=':', linewidth=1)
+
+        def get_pose_gt_and_est():
+            class_id = assert_single_class_and_get_id()
+            class_label = self._class_map.label_from_id(class_id)
+            anno = anno_lookup[class_id]
+            P_gt = np.concatenate([anno.rotation, anno.location.numpy()[:,None]], axis=1)
+            if len(detections) > 0:
+                det = detections[0]
+                P_est = det.pose_results['P_est'] if det.pose_results is not None else None
+            else:
+                P_est = None
+            return P_gt, P_est
+
+        def pose_eval_pretty_print(P_gt, P_est):
+            if P_est is None:
+                return 'Estimation failed.'
+            else:
+                deg_error, cm_error = deg_cm_error(P_est[:,:3], P_est[:,[3]], P_gt[:,:3], P_gt[:,[3]], rescale2meter_factor=1e-3)
+                return '{:.4} cm, {:.4} deg'.format(cm_error, deg_error)
+
 
         def expand_bbox(bbox2d, resize_factor):
             x1, y1, x2, y2 = bbox2d
@@ -114,20 +161,28 @@ class Visualizer:
         img = image_tensor.permute(1, 2, 0)
 
         fig, axes = pyplot.subplots(
-            nrows = 1,
+            nrows = 2,
             ncols = 1,
             squeeze = False,
             figsize = [10, 25],
             # figsize = [dim / PYPLOT_DPI for dim in image_tensor.shape[2:0:-1]],
         )
-        plot_img(axes[0,0], img, 'Both', bbox2d=bbox2d)
-        plot_gt(axes[0,0])
-        plot_det(axes[0,0])
+
+        # Pose
+        class_id = assert_single_class_and_get_id()
+        P_gt, P_est = get_pose_gt_and_est()
+        plot_img(axes[0,0], img, pose_eval_pretty_print(P_gt, P_est), bbox2d=bbox2d)
+        # plot_img(axes[0,0], img, 'Pose close-up', bbox2d=bbox2d)
+        plot_poses(axes[0,0], [class_id], annotations, detections)
+
+        # Keypoints
+        plot_img(axes[1,0], img, 'Both', bbox2d=bbox2d)
+        plot_gt(axes[1,0])
+        plot_det(axes[1,0])
         # plot_img(axes[1,0], img, 'GT', bbox2d=bbox2d)
         # plot_gt(axes[1,0])
         # plot_img(axes[2,0], img, 'Det', bbox2d=bbox2d)
         # plot_det(axes[2,0])
-
 
         self._writer.add_figure(mode, fig, index)
 
